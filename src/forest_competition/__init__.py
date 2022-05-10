230c0c896133a7f4e758d6a8502cfb479b99f0cf
MeSugar <vlasov3454@yandex.ru>
2022-05-11 01:29:50 +0300

    add some basic options to the train module

--- src/forest_competition/__init__.py
+++ src/forest_competition/__init__.py
@@ -1,4 +1,1 @@
-from ensurepip import version
-
-
 __version__ = "0.1.0"
--- src/forest_competition/train.py
+++ src/forest_competition/train.py
@@ -1,12 +1,27 @@
-import click
+from pathlib import Path
+import typing
 
-from . import __version__
+import click
+import pandas as pd
 
 @click.command()
-@click.version_option(version=__version__)
-def main():
-    """Forest competition"""
-    click.print("Hello, world!")
-
-def hi():
-    click.echo("Hi, world!")
+@click.option(
+    "-d",
+    "--dataset-path",
+    default="data/train.csv",  
+    type=click.Path(exists=True,
+                    dir_okay=False,
+                    path_type=Path),
+    help='Path to the training dataset.',
+    show_default=True
+)
+@click.option(
+    "-s",
+    "--save-model-path",
+    default="model/model.joblib",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
+)
+def train(dataset_path: Path) -> None:
+    dataset = pd.read_csv(dataset_path)
+    click.echo(f"Dataset shape: {dataset.shape}.")
