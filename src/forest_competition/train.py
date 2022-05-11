17820c4d2b9808cd1bfbbc97eb272e202ecef5e2
MeSugar <vlasov3454@yandex.ru>
2022-05-11 07:26:56 +0300

    add options for create_pipeline

--- src/forest_competition/train.py
+++ src/forest_competition/train.py
@@ -4,6 +4,10 @@ import typing
 import click
 import pandas as pd
 
+from .get_data import get_data
+from .create_pipeline import create_pipeline
+
+
 @click.command()
 @click.option(
     "-d",
@@ -12,7 +16,6 @@ import pandas as pd
     type=click.Path(exists=True,
                     dir_okay=False,
                     path_type=Path),
-    help='Path to the training dataset.',
     show_default=True
 )
 @click.option(
@@ -22,6 +25,46 @@ import pandas as pd
     type=click.Path(dir_okay=False, writable=True, path_type=Path),
     show_default=True,
 )
-def train(dataset_path: Path) -> None:
-    dataset = pd.read_csv(dataset_path)
-    click.echo(f"Dataset shape: {dataset.shape}.")
+@click.option(
+    "--random-state",
+    default=42,
+    type=int,
+    show_default=True,
+)
+@click.option(
+    "--test-split-ratio",
+    default=0.3,
+    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+    show_default=True,
+)
+@click.option(
+    "--max_iter",
+    default=100,
+    type=int,
+    show_default=True,
+)
+@click.option(
+    "--max_iter",
+    default=100,
+    type=int,
+    show_default=True,
+)
+@click.option(
+    "--logreg-C",
+    default=1.0,
+    type=float,
+    show_default=True,
+)
+def train(
+        dataset_path: Path,
+        save_model_path: Path,
+        random_state: int,
+        test_split_ratio : float
+) -> None:
+    """Script to train and save model."""
+    X_train, X_test, y_train, y_test = get_data(
+        dataset_path,
+        random_state,
+        test_split_ratio,
+    )
+
