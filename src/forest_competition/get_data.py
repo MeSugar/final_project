6fcc9a55c453d32374514da6d2121a9d4c32341b
MeSugar <vlasov3454@yandex.ru>
2022-05-11 07:27:35 +0300

    get_data modified

--- src/forest_competition/get_data.py
+++ src/forest_competition/get_data.py
@@ -11,10 +11,10 @@ def get_data(dataset_path: Path,
                 test_split_ratio: float
 ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
     data = pd.read_csv(dataset_path)
-    click.echo(f"Dataset shape: {dataset.shape}.")
+    click.echo(f"Dataset shape: {data.shape}.")
     X = data.drop("Cover_Type", axis=1)
     y = data["Cover_Type"]
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=test_split_ratio, random_state=random_state
     )
-    return Tuple([X_train, X_test, y_train, y_test])
+    return X_train, X_test, y_train, y_test
