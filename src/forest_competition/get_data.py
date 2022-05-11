810fe1b9b58b16d75de54426d3e9cf001eadc1b6
MeSugar <vlasov3454@yandex.ru>
2022-05-11 06:17:02 +0300

    add get_data script

---
+++ src/forest_competition/get_data.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from typing import Tuple
+
+import click
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def get_data(dataset_path: Path,
+                random_state: int,
+                test_split_ratio: float
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    data = pd.read_csv(dataset_path)
+    click.echo(f"Dataset shape: {dataset.shape}.")
+    X = data.drop("Cover_Type", axis=1)
+    y = data["Cover_Type"]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_split_ratio, random_state=random_state
+    )
+    return Tuple([X_train, X_test, y_train, y_test])
