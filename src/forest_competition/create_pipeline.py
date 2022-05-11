ff6aef92deedcee75947efede62303575a1ed709
MeSugar <vlasov3454@yandex.ru>
2022-05-11 07:04:07 +0300

    add create_pipeline script

---
+++ src/forest_competition/create_pipeline.py
@@ -0,0 +1,16 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+def create_pipeline(
+    max_iter: int,
+    C: float,
+    random_state: int
+) -> Pipeline:
+    pipeline = make_pipeline(
+        StandartScaler(),
+        LogisticRegression(
+            max_iter=max_iter,
+            C=C,
+            random_state=random_state))
+    return pipeline
