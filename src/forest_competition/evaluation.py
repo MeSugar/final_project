9394c65e699586d272e21cdb335c98bca2b884ca
MeSugar <vlasov3454@yandex.ru>
2022-05-12 19:33:53 +0300

    add evaluation module

---
+++ src/forest_competition/evaluation.py
@@ -0,0 +1,26 @@
+import click
+import pandas as pd
+
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import make_scorer
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_auc_score
+from sklearn.pipeline import Pipeline
+
+def evaluate(
+    pipeline : Pipeline,
+    X_test : pd.DataFrame,
+    y_test : pd.Series
+) -> None:
+    scoring = {
+        'accuracy_score' : make_scorer(accuracy_score),
+        'f1_score' : make_scorer(f1_score, average='weighted'),
+        'roc_auc_score' : make_scorer(roc_auc_score, multi_class='ovr', needs_proba=True)
+    }
+    for key in scoring:
+        score  = cross_val_score(
+        pipeline, X_test, y_test, cv=StratifiedKFold(n_splits=5),
+        scoring=scoring[key]).mean()
+        click.echo(f"{key}: {score}")
--- src/forest_competition/train.py
+++ src/forest_competition/train.py
@@ -1,18 +1,12 @@
-from pathlib import Path
-
-from joblib import dump
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import make_scorer
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import f1_score
-from sklearn.metrics import roc_auc_score
-
 import click
 import pandas as pd
 
+from pathlib import Path
+from joblib import dump
+
 from .data import get_data
 from .pipeline import create_pipeline
+from .evaluation import evaluate
 
 
 @click.command()
@@ -80,16 +74,8 @@ def train(
     )
     pipeline = create_pipeline(n_neighbors, knn_weights)
     pipeline.fit(X_train, y_train)
-    scoring = {
-        'accuracy_score' : make_scorer(accuracy_score),
-        'f1_score' : make_scorer(f1_score, average='weighted'),
-        'roc_auc_score' : make_scorer(roc_auc_score, multi_class='ovr', needs_proba=True)
-    }
-    for key in scoring:
-        score  = cross_val_score(
-        pipeline, X_test, y_test, cv=StratifiedKFold(n_splits=5),
-        scoring=scoring[key]).mean()
-        click.echo(f"{key}: {score}")
+    evaluate(pipeline, X_test, y_test)
+
     path_folder = save_model_path.parent
     path_folder.mkdir(exist_ok=True)
     save_model_path.unlink(missing_ok=True)
