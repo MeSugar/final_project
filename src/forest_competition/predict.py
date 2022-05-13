c91a058bc59883a17f8d54d7916fd535569d2565
MeSugar <vlasov3454@yandex.ru>
2022-05-13 23:19:23 +0300

    add predict script

--- src/forest_competition/predict.py
+++ src/forest_competition/predict.py
@@ -1,22 +1,75 @@
 import click
 import pandas as pd
+import numpy as np
 
 from pathlib import Path
+from joblib import load
 from sklearn.pipeline import Pipeline
+
+@click.command()
+@click.option(
+    "-d",
+    "--test-dataset-path",
+    default="data/train.csv",
+    type=click.Path(exists=True,
+                    dir_okay=False,
+                    path_type=Path),
+    show_default=True,
+    help="Path to the test dataset file."
+)
+@click.option(
+    "-s",
+    "--sample-submission-path",
+    default="data/sampleSubmission.csv",
+    type=click.Path(exists=True,
+                    dir_okay=False,
+                    path_type=Path),
+    show_default=True,
+    help="Path to the sample submission file."
+)
+@click.option(
+    "-m",
+    "--model-path",
+    default="model/model.joblib",
+    type=click.Path(exists=True,
+                    dir_okay=False,
+                    path_type=Path),
+    show_default=True,
+    help="Path to the model."
+)
+@click.option(
+    "-p",
+    "--pipeline",
+    default="model/pipeline.joblib",
+    type=click.Path(exists=True,
+                    dir_okay=False,
+                    path_type=Path),
+    show_default=True,
+    help="Path to the data preprocessing pipeline."
+)
+@click.option(
+    "--save-predictions-path",
+    default="data/predictions.csv",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
+    help="Path to save the predictions file."
+)
 
 def predict(
     test_dataset_path : Path,
     sample_submission_path : Path,
-    save_predictions_path : Path,
-    pipeline : Pipeline
+    model_path : Path,
+    pipeline_path : Path,
+    save_predictions_path : Path
 ) -> None:
     X_test = pd.read_csv(test_dataset_path, index_col="Id")
-    predictions = pipeline.predict(X_test)
+    pipeline = load(pipeline_path)
+    new_X = pipeline.transform(np.array(X_test))
+    model = load(model_path)
+    predictions = model.predict(new_X)
     submission = pd.read_csv(sample_submission_path)
     submission["Cover_Type"] = predictions
     #saving predcitions
-    path_folder = save_predictions_path.parent
-    path_folder.mkdir(exist_ok=True)
     save_predictions_path.unlink(missing_ok=True)
     submission.to_csv(save_predictions_path, index=False)
     click.echo(f"Predictions is saved to {save_predictions_path}.")
