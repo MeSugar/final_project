479ad1623740078eb250d6390a399b3343e0c3ca
Eugene Vlasov <vlasov3454@yandex.ru>
2022-05-13 17:40:46 +0300

    add options for lr and rf classifiers

--- src/forest_competition/train.py
+++ src/forest_competition/train.py
@@ -9,6 +9,7 @@ from joblib import dump
 from .data import get_data
 from .pipeline import create_pipeline
 from .evaluation import evaluate
+from .model import generate_model
 
 
 @click.command()
@@ -49,7 +50,14 @@ from .evaluation import evaluate
     default=True,
     type=bool,
     show_default=True,
-    help="Option to use StandardScaler to normalize numeric parameters."
+    help="Use StandardScaler to standertize numeric parameters."
+)
+@click.option(
+    "--use-pca",
+    default=False,
+    type=bool,
+    show_default=True,
+    help="Use PCA to reduce dimensionality."
 )
 @click.option(
     "--n-neighbors",
@@ -62,7 +70,7 @@ from .evaluation import evaluate
     "--knn-weights",
     default='uniform',
     type=click.Choice(["uniform", "distance"]),
-    show_default="uniform",
+    show_default=True,
 )
 @click.option(
     "--lr-c",
@@ -72,7 +80,28 @@ from .evaluation import evaluate
     help="Inverse of regularization strength used by LogisticRegression algorithm."
 )
 @click.option(
-    "--algorithm",
+    "--n-estimators",
+    default=100,
+    type=int,
+    show_default=True,
+    help="The number of trees in the random forest."
+)
+@click.option(
+    "--criterion",
+    default='gini',
+    type=click.Choice(["gini", "entropy", "log_loss"]),
+    show_default=True,
+    help="Random forest function to measure the quality of a split."
+)
+@click.option(
+    "--max-depth",
+    default=None,
+    type=int,
+    show_default=True,
+    help="The maximum depth of the random forest tree."
+)
+@click.option(
+    "--classifier",
     default="knn",
     type=click.Choice(["knn", "lr", "rf"]),
     show_default=True,
@@ -85,10 +114,14 @@ def train(
         random_state: int,
         test_split_ratio : float,
         use_scaler : bool,
+        use_pca : bool,
         n_neighbors : int,
         knn_weights : str,
         lr_c : float,
-        algorithm : str
+        n_estimators : str,
+        criterion : str,
+        max_depth : int,
+        classifier : str
 ) -> None:
     """Script to train and save model."""
     X_train, X_test, y_train, y_test = get_data(
@@ -96,16 +129,30 @@ def train(
         random_state,
         test_split_ratio,
     )
-    pipeline = create_pipeline(n_neighbors, knn_weights)
+    #generate model object
+    clf = generate_model(
+        classifier, random_state,
+        n_neighbors, knn_weights,
+        lr_c, n_estimators,
+        criterion, max_depth
+    )
+    #pipeline
+    pipeline = create_pipeline(
+        use_scaler, use_pca,
+        X_train.iloc[:, :10].columns,
+        classifier
+    )
     pipeline.fit(X_train, y_train)
     scores = evaluate(pipeline, X_test, y_test)
     # logging
     with mlflow.start_run():
         mlflow.log_metrics(scores)
+        mlflow.log_param("classifier", classifier)
         mlflow.log_param("use_scaler", use_scaler)
+        mlflow.log_param("use_pca", use_pca)
         mlflow.log_param("n_neighbors", n_neighbors)
         mlflow.log_param("knn_weights", knn_weights)
-        mlflow.sklearn.log_model(pipeline, "model")
+        mlflow.sklearn.log_model(pipeline, "cla")
     #saving model
     path_folder = save_model_path.parent
     path_folder.mkdir(exist_ok=True)
