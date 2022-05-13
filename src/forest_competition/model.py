840086f7568f07387b9d355846c9143c7585d07c
MeSugar <vlasov3454@yandex.ru>
2022-05-13 17:30:05 +0300

    add model func to generate model

---
+++ src/forest_competition/model.py
@@ -0,0 +1,34 @@
+from typing import Any
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+
+def generate_model(
+    classifier : str,
+    random_state : int,
+    n_neighbors : int,
+    knn_weights : str,
+    lr_c : float,
+    n_estimators : int,
+    criterion : str,
+    max_depth : int
+) -> Any:
+    if classifier == 'knn':
+        return KNeighborsClassifier(
+            n_neighbors=n_neighbors,
+            knn_weights=knn_weights,
+            random_state=random_state)
+    elif classifier == 'lr':
+        return LogisticRegression(
+            C=lr_c,
+            random_state=random_state
+        )
+    else:
+        return RandomForestClassifier(
+            n_estimators=n_estimators,
+            criterion=criterion,
+            max_depth=max_depth
+        )
+
+
