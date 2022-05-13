15b74e374de95e7f5b61200ec6f78b2c635408ba
Eugene Vlasov <vlasov3454@yandex.ru>
2022-05-13 17:39:35 +0300

    add pca to the pipeline

--- src/forest_competition/pipeline.py
+++ src/forest_competition/pipeline.py
@@ -1,16 +1,28 @@
-from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+
 from sklearn.pipeline import make_pipeline
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.decomposition import PCA
+
 
 def create_pipeline(
-    n_neighbors : int,
-    knn_weights : str
+    use_scaler : bool,
+    use_pca : bool,
+    columns_to_scale : pd.DataFrame,
+    classifier
 ) -> Pipeline:
+    steps = []
+    if use_scaler:
+        steps.append(("scaler", StandardScaler(), columns_to_scale))
+    if use_pca:
+        steps.append(("pca", PCA(n_components=5), columns_to_scale))
+    preprocessor = ColumnTransformer(
+    steps,
+    remainder="passthrough"
+    )
     pipeline = make_pipeline(
-        StandardScaler(),
-        KNeighborsClassifier(
-            n_neighbors=n_neighbors,
-            weights=knn_weights
-            ))
+        preprocessor,
+        classifier)
     return pipeline
