b2e81d4f895ff3a0e97fe9d6e87948a7eb0ebfae
MeSugar <vlasov3454@yandex.ru>
2022-05-16 21:55:07 +0300

    remove train/test split

--- src/forest_competition/data.py
+++ src/forest_competition/data.py
@@ -6,14 +6,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-def get_data(dataset_path: Path,
-                random_state: int,
-                test_split_ratio: float
+def get_data(
+    dataset_path: Path,
+    random_state : int
 ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
     data = pd.read_csv(dataset_path, index_col='Id')
     click.echo(f"Dataset shape: {data.shape}.")
     X = data.drop("Cover_Type", axis=1)
     y = data["Cover_Type"]
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=test_split_ratio, random_state=random_state, stratify=y)
-    return X_train, X_test, y_train, y_test
+    return X, y
