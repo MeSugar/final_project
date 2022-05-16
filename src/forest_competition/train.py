deb2f514fece54fb7e978cc9153fcc268150bb6d
MeSugar <vlasov3454@yandex.ru>
2022-05-16 21:57:09 +0300

    add model evaluation & tuning

--- src/forest_competition/train.py
+++ src/forest_competition/train.py
@@ -1,3 +1,4 @@
+import pip
 import click
 import mlflow
 import mlflow.sklearn
@@ -9,9 +10,8 @@ from pathlib import Path
 from joblib import dump
 
 from .data import get_data
-from .pipeline import create_pipeline
-from .evaluation import evaluate
-from .model import generate_model
+from .pipeline import build_pipeline
+from .model import init_classifier, model_evaluation, model_tuning
 from .predict import predict
 
 
@@ -38,36 +38,11 @@ from .predict import predict
     help="Path where the fitted model will be saved."
 )
 @click.option(
-    "-p",
-    "--save-pipeline-path",
-    default="model/pipeline.joblib",
-    type=click.Path(
-        dir_okay=False,
-        writable=True,
-        path_type=Path),
-    show_default=True,
-    help="Path to the data preprocessing pipeline."
-)
-@click.option(
     "--random-state",
     default=42,
     type=int,
     show_default=True,
     help="Parameter to control the random number generator used by algorithms."
-)
-@click.option(
-    "--test-split-ratio",
-    default=0.3,
-    type=click.FloatRange(0, 1, min_open=True, max_open=True),
-    show_default=True,
-    help="Fraction of splitting the dataset for training and testing a model."
-)
-@click.option(
-    "--use-scaler",
-    default=True,
-    type=bool,
-    show_default=True,
-    help="Use StandardScaler to standertize numeric parameters."
 )
 @click.option(
     "--use-pca",
@@ -77,109 +52,56 @@ from .predict import predict
     help="Use PCA to reduce dimensionality."
 )
 @click.option(
-    "--n-neighbors",
-    default=5,
-    type=int,
+    "--use-boruta",
+    default=False,
+    type=bool,
     show_default=True,
-    help="Number of neighbors used by KNN algorithm."
-)
-@click.option(
-    "--knn-weights",
-    default='uniform',
-    type=click.Choice(["uniform", "distance"]),
-    show_default=True,
-)
-@click.option(
-    "--lr-c",
-    default=1.0,
-    type=float,
-    show_default=True,
-    help="Inverse of regularization strength used by LogisticRegression algorithm."
-)
-@click.option(
-    "--n-estimators",
-    default=100,
-    type=int,
-    show_default=True,
-    help="The number of trees in the random forest."
-)
-@click.option(
-    "--criterion",
-    default='gini',
-    type=click.Choice(["gini", "entropy"]),
-    show_default=True,
-    help="Random forest function to measure the quality of a split."
-)
-@click.option(
-    "--max-depth",
-    default=None,
-    type=int,
-    show_default=True,
-    help="The maximum depth of the random forest tree."
+    help="Use Boruta for feature selection."
 )
 @click.option(
     "--classifier",
     default="knn",
-    type=click.Choice(["knn", "lr", "rf"]),
+    type=click.Choice(["knn", "logreg", "rfc"]),
     show_default=True,
     help="Algorithm to be used for modeling."
 )
-
 def train(
         dataset_path: Path,
         save_model_path: Path,
-        save_pipeline_path : Path,
         random_state: int,
-        test_split_ratio : float,
-        use_scaler : bool,
         use_pca : bool,
-        n_neighbors : int,
-        knn_weights : str,
-        lr_c : float,
-        n_estimators : str,
-        criterion : str,
-        max_depth : int,
+        use_boruta : bool,
         classifier : str
 ) -> None:
     """Script to train and save model."""
-    X_train, X_test, y_train, y_test = get_data(
+    X, y = get_data(
         dataset_path,
         random_state,
-        test_split_ratio,
     )
-    #generate model object
-    clf = generate_model(
-        classifier, random_state,
-        n_neighbors, knn_weights,
-        lr_c, n_estimators,
-        criterion, max_depth
+    clf = init_classifier(classifier, random_state)
+    pipeline = build_pipeline(
+        use_pca, use_boruta,
+        slice(0, 10), clf
     )
-    #preprocessing pipeline
-    pipeline = create_pipeline(
-        use_scaler, use_pca,
+    scores = model_evaluation(pipeline, classifier, X, y)
+    click.echo(scores)
+    params = model_tuning(pipeline, classifier, X, y)
+    final_model = build_pipeline(
+        use_pca, use_boruta,
         slice(0, 10),
-        save_pipeline_path
+        init_classifier(classifier, random_state)
     )
-    new_X_train = pipeline.fit_transform(np.array(X_train))
-    #save pipeline
-    path_folder = save_pipeline_path.parent
-    path_folder.mkdir(exist_ok=True)
-    save_pipeline_path.unlink(missing_ok=True)
-    dump(pipeline, save_pipeline_path)
-    click.echo(f"Pipeline is saved to {save_pipeline_path}.")
-    #fitting clf
-    clf.fit(new_X_train, y_train)
-    new_X_test = pipeline.transform(np.array(X_test))
-    scores = evaluate(clf, new_X_test, y_test)
+    final_model.set_params(**params)
+    final_model.fit(X, y)
     # logging
     with mlflow.start_run():
         mlflow.log_metrics(scores)
-        mlflow.log_params(clf.get_params())
+        mlflow.log_params(params)
         mlflow.log_param("classifier", classifier)
-        mlflow.log_param("use_scaler", use_scaler)
         mlflow.log_param("use_pca", use_pca)
+        mlflow.log_param("use_boruta", use_boruta)
         mlflow.sklearn.log_model(pipeline, "cla")
-    #saving model
+    #saving the model
     path_folder = save_model_path.parent
     path_folder.mkdir(exist_ok=True)
     save_model_path.unlink(missing_ok=True)
